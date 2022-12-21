984f78536baf12bc8fb464d3a239bca2da21b275
ajay <ajaynairarun@gmail.com>
2022-12-21 12:43:09 +0530

    change : folder names

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import { binarySearch } from './lib/binary-search';
-import { findDuplicate } from './lib/binary-search/duplicate.number';
-import { findMin } from './lib/binary-search/find.min';
-import { searchMatrix } from './lib/binary-search/search.a.2d.matrix';
-import { search } from './lib/binary-search/search.in-rotated.sorted.array.2';
-import { totalFruit } from './lib/sliding-window/fruit.into.baskets';
-import { minSubArrayLen } from './lib/sliding-window/minimum.size.subarray';
 
-// const answer = binarySearch();
-// const answerToDuplicateNumber = findDuplicate();
-// const answerToFindMin = findMin();
-// const answerToSearchInRotatedArray = search();
-// const answerToRotatedArray2 = search([1, 0, 1, 1, 1], 2);
-// const answerToSearchMatrix = searchMatrix(
-//   [
-//     [1, 3, 5, 7],
-//     [10, 11, 16, 20],
-//     [23, 30, 34, 60]
-//   ],
-//   3
-// );
-// const answerToMinSubArr = minSubArrayLen(7, [2, 3, 1, 2, 4, 3]);
-// const maxFruits = totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]);
+
+
