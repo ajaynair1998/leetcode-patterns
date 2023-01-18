f063d80a6155776f0811e8b9ec51e58be77fc8af
ajay <ajaynairarun@gmail.com>
2023-01-18 13:52:16 +0530

    add : clone graph bfs method

--- src/lib/graph/clone.graph.ts
+++ src/lib/graph/clone.graph.ts
@@ -36,3 +36,32 @@ const dfs = (node: Node | null, map: { [key: string]: Node }): Node | null => {
     return null;
   }
 };
+
+const bfs = (node: Node | null, map: { [key: string]: Node }): Node | null => {
+  try {
+    if (!node) {
+      return null;
+    } else {
+      let queue = [node];
+      let firstNode = new Node(node.val);
+      map[node.val] = firstNode;
+      while (queue.length > 0) {
+        let leftMostNode = queue.shift();
+        if (!leftMostNode) {
+          continue;
+        }
+        for (let item of leftMostNode.neighbors) {
+          if (!map[item.val]) {
+            map[item.val] = new Node(item.val);
+            queue.push(item);
+          }
+          map[leftMostNode.val].neighbors.push(map[item.val]);
+        }
+      }
+      return firstNode;
+    }
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
